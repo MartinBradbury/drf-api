915fa491f166cfe4634d63f21424d00a4a62f8b5
Martin Bradbury <martinbradbury@btinternet.com>
2024-05-31 16:42:30 +0000

    corrected error and reverted debug back

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,10 +58,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# 'DEV' in os.environ
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-martinbradbury-drfapi-rusmjvyofvc.ws-eu114.gitpod.io' 'drf_api.herokuapp.com']
+ALLOWED_HOSTS = ['8000-martinbradbury-drfapi-rusmjvyofvc.ws-eu114.gitpod.io' 'drf-api-first.herokuapp.com']
 
 
 # Application definition
